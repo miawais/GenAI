35e9bf9f662ae1a4f5a2a987e231c9531da6fdf6
Muhammad Awais <210633@students.au.edu.pk>
2025-03-15 18:03:52 +0500

    Google API Configuration Done

--- ChatDB/app.py
+++ ChatDB/app.py
@@ -4,6 +4,8 @@ load_dotenv()
 import streamlit as st
 import os
 import sqlite3
-import google.generativeai as generativeai
+import google.generativeai as genai
 
 #configuring api key
+genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
+
